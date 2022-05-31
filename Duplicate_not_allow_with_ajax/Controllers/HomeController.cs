2eaa46f19d8a8ab1781f3e8e962fe998941aae7b
Muhammad Sahal Qasim <sahal.qasim@premier.com.pk>
2022-05-31 15:20:43 +0500

    Duplicate not allow with ajax in MVC

---
+++ Duplicate_not_allow_with_ajax/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Duplicate_not_allow_with_ajax.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Duplicate_not_allow_with_ajax.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        MVCEntities db = new MVCEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult CheckUsernameAvailability(string userdata)
+        {
+            System.Threading.Thread.Sleep(200);
+            var SeachData = db.StudentDetails.Where(x => x.StuName == userdata).SingleOrDefault();
+            if (SeachData != null)
+            {
+                return Json(1);
+            }
+            else
+            {
+                return Json(0);
+            }
+
+        }
+    }
+}
---
+++ Duplicate_not_allow_with_ajax/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Duplicate_not_allow_with_ajax.MvcApplication" Language="C#" %>
